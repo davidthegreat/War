809b0950a781a56cd4bb9ef481accaf9aa901a5c
Macbook Air <macbookair@David-Ramirez.netgear.com>
2016-09-27 15:04:16 -0700

    add function to generate a random card

--- War/ViewController.swift
+++ War/ViewController.swift
@@ -26,7 +26,22 @@ class ViewController: UIViewController {
     }
 
     @IBAction func playRoundTapped(sender: AnyObject) {
-        self.playRoundButton.setTitle("Play Round", forState: .Normal)
+        
+        // Randomize a number for the first imageview
+        let firstRandomNumber = arc4random_uniform(13) + 1
+        
+        //construct a string with random number
+        let firstCardString:String = String(format: "card%i", firstRandomNumber)
+        let secondRandomNumber = arc4random_uniform(13) + 1
+        
+        let secondCardString:String = String(format: "card%i", secondRandomNumber)
+        
+        //Set the first card image view to the assest corresponding to the randomized number
+        self.firstCardImageView.image = UIImage(named: firstCardString)
+      
+        
+        
+        self.secondCardImageView.image = UIImage(named: secondCardString)
     }
 
 }
