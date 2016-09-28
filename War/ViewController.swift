8593ed9086e56b29c0f0ae69c24edc89aef06bf7
Macbook Air <macbookair@David-Ramirez.netgear.com>
2016-09-27 21:08:18 -0700

    add a playbutton and delete the play button text

--- War/ViewController.swift
+++ War/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.playRoundButton.setTitle("Play", forState: .Normal)
+       
     }
 
     override func didReceiveMemoryWarning() {
